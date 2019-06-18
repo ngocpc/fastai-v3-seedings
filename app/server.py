dd5ebc4e505fee17232f09e0bfb77463118317de
biosys <phamcamngoc@gmail.com>
2019-06-18 13:48:36 +0200

    server updated

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1WjR1_Qw0m603BDxi9zIq76Xtw4f6A00n'
 export_file_name = 'export.pkl'
 
 classes = ['Black-grass',
