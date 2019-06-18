394fa55bd4b4a36feb1446884db8b5eb032e7113
biosys <phamcamngoc@gmail.com>
2019-06-18 12:19:12 +0200

    third

--- app/server.py
+++ app/server.py
@@ -12,7 +12,19 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Black-grass',
+ 'Charlock',
+ 'Cleavers',
+ 'Common Chickweed',
+ 'Common wheat',
+ 'Fat Hen',
+ 'Loose Silky-bent',
+ 'Maize',
+ 'Scentless Mayweed',
+ 'Shepherds Purse',
+ 'Small-flowered Cranesbill',
+ 'Sugar beet']
+
 path = Path(__file__).parent
 
 app = Starlette()
